4f2211a3105093afe17c640157e0370b86c41ec7
Aashni Shah <contact@aashni.me>
2015-09-20 09:05:18 -0400

    add firebase integration on link clicks

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -25,4 +25,14 @@ angular.module('myApp.view1', ['ngRoute'])
   // synchronize the object with a three-way data binding
   // click on `index.html` above to see it used in the DOM!
   syncObject.$bindTo($scope, "data");
+
+  $scope.increaseCount = function(id) {
+    ref.transaction(function(current) {
+      if( !current ) {
+        current = '';
+      }
+      current[id] = (current[id] || 0) + 1;
+      return current;
+    });
+  };
 });
--- app/view2/view2.js
+++ app/view2/view2.js
@@ -9,7 +9,7 @@ angular.module('myApp.view2', ['ngRoute'])
   });
 }])
 
-.controller('View2Ctrl', function($scope, $routeParams, $http) {
+.controller('View2Ctrl', function($scope, $routeParams, $http, $firebaseObject) {
   var url = 'http://congen.co/clickbait/test.json';
   $http.get(url).then(function(content) {
     $scope.articles = content.data;
@@ -18,4 +18,21 @@ angular.module('myApp.view2', ['ngRoute'])
     });
     $scope.pineapple = $scope.articles[$routeParams.id];
   });
+
+  var ref = new Firebase("https://boiling-fire-8955.firebaseio.com");
+  // download the data into a local object
+  var syncObject = $firebaseObject(ref);
+  // synchronize the object with a three-way data binding
+  // click on `index.html` above to see it used in the DOM!
+  syncObject.$bindTo($scope, "data");
+
+  $scope.increaseCount = function(id) {
+    ref.transaction(function(current) {
+      if( !current ) {
+        current = '';
+      }
+      current[id] = (current[id] || 0) + 1;
+      return current;
+    });
+  };
 });
