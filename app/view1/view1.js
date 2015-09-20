68d33ff15b4b852cc7c8a5cec714ccec6e2f89dc
Aashni Shah <contact@aashni.me>
2015-09-20 05:21:32 -0400

    voodoo, things show up

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -10,17 +10,16 @@ angular.module('myApp.view1', ['ngRoute'])
 
 }])
 
-.controller('View1Ctrl', function($scope, $firebaseObject, $http) {
-  var url = 'http://congen.co/clickbait/test.txt?callback=JSON_CALLBACK';
-  $http.jsonp(url).success(function(content) {
-    $scope.articles = content;
+.controller('View1Ctrl', function($scope, $http) {
+  var url = 'http://congen.co/clickbait/test.json?callback=JSON_CALLBACK';
+  $http.get(url).then(function(content) {
+    $scope.articles = content.data;
   });
-  console.log($scope.articles);
 
-  var ref = new Firebase("https://boiling-fire-8955.firebaseio.com");
+  /*var ref = new Firebase("https://boiling-fire-8955.firebaseio.com");
   // download the data into a local object
   var syncObject = $firebaseObject(ref);
   // synchronize the object with a three-way data binding
   // click on `index.html` above to see it used in the DOM!
-  syncObject.$bindTo($scope, "data");
+  syncObject.$bindTo($scope, "data");*/
 });
